686a2bed261f30b0fe030868caf1b1dd2ecd359e
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-09-02 00:42:42 +0300

    style buttons with shadow properties

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,15 +1,35 @@
 import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
+import PrimaryButton from '../components/PrimaryButton'
 
 const StartGameScreen = () => {
     return (
-        <View>
+        <View style={styles.inputContainer}>
             <TextInput />
-            <Button title={""} />
+
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
+
         </View>
     )
 }
 
 export default StartGameScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        padding: 16,
+        marginHorizontal: 24,
+        backgroundColor: "#72063c",
+        borderRadius: 8,
+        elevation: 4, // shadow for android
+        shadowColor: "black", // shadow for ios
+        shadowOffset: { // shadow sizes
+            width: 0,
+            height: 2
+        },
+        shadowRadius: 6, // shadow expands
+        shadowOpacity: 0.25 // how strong shadow is
+    },
+})
