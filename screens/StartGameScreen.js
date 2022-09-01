92c5ec57a2bce2c0c7e91c0ae291bc948c693e11
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-09-02 02:02:20 +0300

    configuration TextInput field

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,8 +5,13 @@ import PrimaryButton from '../components/PrimaryButton'
 const StartGameScreen = () => {
     return (
         <View style={styles.inputContainer}>
-            <TextInput />
-
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType={"number-pad"}
+                autoCorrect={false}
+                autoCapitalize={"none"}
+            />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
 
@@ -32,4 +37,15 @@ const styles = StyleSheet.create({
         shadowRadius: 6, // shadow expands
         shadowOpacity: 0.25 // how strong shadow is
     },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb63f',
+        borderBottomWidth: 2,
+        color: '#ddb63f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: "center",
+    }
 })
