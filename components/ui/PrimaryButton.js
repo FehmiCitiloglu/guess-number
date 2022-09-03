d3fffe11404951a0680548035201912ef6764924
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-09-03 17:50:16 +0300

    reconstruct component folder structure

--- components/ui/PrimaryButton.js
+++ components/ui/PrimaryButton.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, Pressable } from 'react-native'
 import React from 'react'
-import Colors from '../constants/colors'
+import Colors from '../../constants/colors'
 
 
 
